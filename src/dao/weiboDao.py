59c031587b178efe5fe592d399735897f64f8783
244267620@qq.com <wlc68911qet>
2020-04-24 16:31:48 +0800

    update

--- src/dao/weiboDao.py
+++ src/dao/weiboDao.py
@@ -28,20 +28,21 @@ class WeiboDao:
 		page_content = reptile.get_page_content(url, is_ua=True, timeout=3)
 		content = BeautifulSoup(page_content, "html.parser")
 		hot_data = content.find('div', id='pl_top_realtimehot')
-		hot_list = hot_data.find('tbody').findAll('tr')
+		tr_list = hot_data.find('tbody').findAll('tr')
 
 		# 3.内容格式化
 		data = []
-		for index, tr in enumerate(hot_list):
+		prefix = 'https://s.weibo.com'
+		for index, tr in enumerate(tr_list):
 			td_list = tr.findAll('td')
-			# hot = {
-			# 	'index': index,
-			# 	'title': item['target']['titleArea']['text'],
-			# 	'abstract': item['target']['excerptArea']['text'],
-			# 	'url': item['target']['link']['url'],
-			# 	'heat': item['target']['metricsArea']['text'],
-			# 	'imgae': item['target']['imageArea']['url']
-			# }
-			# data.append(hot)
+			hot = {
+				'index': index,
+				'title': td_list[1].a.text,
+				'url': prefix + td_list[1].a.get('href'),
+				'heat': 'None' if td_list[1].span is None else td_list[1].span.text,
+				'emotion': 'None' if td_list[1].img is None else td_list[1].img.get('title'),
+				'type': 'None' if td_list[2].i is None else td_list[2].i.text,
+			}
+			data.append(hot)
 
 		return data
